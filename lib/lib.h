cdf92aee6e089ce667dae3d226285341c7f82b93
dannytheway <daniel.fragrante@gmail.com>
2025-04-15 12:34:01 +0200

    gitignore, init main, time utils setup

--- lib/lib.h
+++ lib/lib.h
@@ -5,4 +5,7 @@
 
 double measure_function_run(void *f, unsigned int *arr, unsigned int len);
 
+double getResolution();
+double duration(struct timespec start, struct timespec end);
+
 #endif
--- lib/time_measuraments.c
+++ lib/time_measuraments.c
@@ -11,3 +11,15 @@ double measure_function_run(void *f, unsigned int *arr, unsigned int len){
     return elapsed;
 }
 
+double duration(struct timespec start, struct timespec end) {
+    return end.tv_sec - start.tv_sec 
+           + ((end.tv_nsec - start.tv_nsec ) / (double) 1000000000.0);
+}
+double getResolution() {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    do {
+        clock_gettime(CLOCK_MONOTONIC, &end);
+    } while (duration(start, end) == 0.0);
+    return duration(start, end);
+}
