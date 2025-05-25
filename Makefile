58523f9da680d0319ba268e56cab7fb5af6a336d
dannytheway <daniel.fragrante@gmail.com>
2025-05-25 16:01:01 +0200

    Makefile and file utilities update

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: build
 
 build:
 	@echo "Building chronometer..."
-	gcc -o out/chronometer chronometer.c lib/*.c -I. -Wall
+	gcc -o out/chronometer chronometer.c lib/*.c lib/alg/*.c -I. -Wall
 	@echo "Build finished."
 
 clean:
--- lib/files.c
+++ lib/files.c
@@ -1,5 +1,22 @@
 #include "lib.h"
 
-void write_dataline(FILE *file, double time, int data) {
+void write_dataline(FILE *file, double time) {
+    fprintf(file, "%lf\n", time);
+}
+
+// I dont remember what data is for -_-
+void write_dataline_ahh(FILE *file, double time, int data) {
     fprintf(file, "%lf, %d\n", time, data);
+}
+
+int write_logfile(char *s, double *times){
+    FILE *f=fopen(s, "w");
+    if(f==NULL){
+        puts("Error opening file");
+        return -1;
+    }
+
+    for(int i=0; i<arr_len(times); i++){
+        write_dataline(f, times[i]);
+    }
 }
